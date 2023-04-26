9431b09f548b271581c07adf72dd3dce0baf10b3
unclexo <itsunclexo@gmail.com>
2023-04-26 17:17:01 +0600

    Tests raising exception for invalid login credentials

--- tests/Feature/Http/Controllers/Auth/AdminControllerTest.php
+++ tests/Feature/Http/Controllers/Auth/AdminControllerTest.php
@@ -29,4 +29,26 @@ class AdminControllerTest extends TestCase
             fn (string $token) => str_starts_with($token, 'eyJ')
         );
     }
+
+    public function test_it_can_raise_exception_for_invalid_credentials_given()
+    {
+        $email = 'admin@buckhill.co.uk';
+        $password = 'admin';
+
+        User::factory()->create([
+            'email' => $email, 'password' => bcrypt($password)
+        ]);
+
+        $this->postJson(route('v1.admin.login'), [
+            'email' => '', 'password' => ''
+        ])
+        ->assertStatus(422)
+        ->assertJsonValidationErrors(['email', 'password']);
+
+        $this->postJson(route('v1.admin.login'), [
+            'email' => 'invalid@example.com', 'password' => '123'
+        ])
+        ->assertStatus(422)
+        ->assertJsonValidationErrors(['email']);
+    }
 }
