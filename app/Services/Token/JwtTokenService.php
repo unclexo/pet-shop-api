ba3cd067d0e7b7b5dbb40aac07463952cf011634
unclexo <itsunclexo@gmail.com>
2023-04-26 12:39:08 +0600

    Tests creating a jwt token

--- app/Services/Token/JwtTokenService.php
+++ app/Services/Token/JwtTokenService.php
@@ -5,7 +5,12 @@ namespace App\Services\Token;
 
 
 use DateTimeImmutable;
+use Lcobucci\JWT\Encoding\ChainedFormatter;
+use Lcobucci\JWT\Encoding\JoseEncoder;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+use Lcobucci\JWT\Signer\Key\InMemory;
 use Lcobucci\JWT\Token;
+use Lcobucci\JWT\Token\Builder;
 
 class JwtTokenService implements Tokenable
 {
@@ -27,7 +32,17 @@ class JwtTokenService implements Tokenable
 
     public function token(): Token
     {
-        // TODO: Implement token() method.
+        $tokenBuilder = (new Builder(new JoseEncoder(), ChainedFormatter::default()));
+        $algorithm    = new Sha256();
+        $signingKey   = InMemory::plainText(random_bytes(32));
+
+        $now   = new DateTimeImmutable();
+
+        return $tokenBuilder
+            ->issuedBy('http://example.com')
+            ->expiresAt($now->modify('+1 hour'))
+            ->withClaim('uid', 1)
+            ->getToken($algorithm, $signingKey);
     }
 
     public function parse(string $token): Token
--- tests/Unit/Services/Token/JwtTokenServiceTest.php
+++ tests/Unit/Services/Token/JwtTokenServiceTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Services\Token;
 
 
 use App\Services\Token\JwtTokenService;
+use Lcobucci\JWT\Token;
 use Tests\TestCase;
 
 class JwtTokenServiceTest extends TestCase
@@ -11,5 +12,11 @@ class JwtTokenServiceTest extends TestCase
     public function test_jwt_token_service_exists_in_service_container()
     {
         $this->assertInstanceOf(JwtTokenService::class, app('jwt'));
+    }
+
+    public function test_it_can_create_jwt_token()
+    {
+        $this->assertInstanceOf(Token::class, $token = app('jwt')->token());
+        $this->assertTrue(str_starts_with($token->toString(), 'eyJ'));
     }
 }
