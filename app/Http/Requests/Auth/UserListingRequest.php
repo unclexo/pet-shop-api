2b2c70cea50de49d3126621c09c0693db256726a
unclexo <itsunclexo@gmail.com>
2023-04-28 23:42:27 +0600

    Fixes unknown column error occurred by sortBy value

--- app/Http/Requests/Auth/UserListingRequest.php
+++ app/Http/Requests/Auth/UserListingRequest.php
@@ -3,10 +3,16 @@
 namespace App\Http\Requests\Auth;
 
 
+use App\Rules\ColumnExists;
+use App\Traits\NeedsCustomResponse;
+use Closure;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Schema;
 
 class UserListingRequest extends FormRequest
 {
+    use NeedsCustomResponse;
+
     /**
      * Determine if the user is authorized to make this request.
      */
@@ -25,7 +31,17 @@ class UserListingRequest extends FormRequest
         return [
             'page' => ['sometimes', 'integer'],
             'limit' => ['sometimes', 'integer'],
-            'sortBy' => ['sometimes', 'string'],
+            'sortBy' => [
+                'sometimes',
+                'string',
+                function (string $attribute, mixed $value, Closure $fail) {
+                    if (! Schema::hasColumn('users', $value)) {
+                        $this->throwHttpResponseException(errors: [
+                            'sortBy' => "The {$value} is an invalid column."]
+                        );
+                    }
+                },
+            ],
             'desc' => ['sometimes', 'boolean'],
         ];
     }
@@ -39,7 +55,7 @@ class UserListingRequest extends FormRequest
             'limit' => $data['limit'] ?? null,
             'sortBy' => $data['sortBy'] ?? 'first_name',
             'desc' => isset($data['desc'])
-                ? $data['desc'] === true ? 'desc' : 'asc'
+                ? $data['desc'] == true ? 'desc' : 'asc'
                 : 'asc',
         ];
     }
