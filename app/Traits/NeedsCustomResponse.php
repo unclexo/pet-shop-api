1f4c4098d5e1f12d1945b087bc6a6f1d9e76ccd9
unclexo <itsunclexo@gmail.com>
2023-04-28 20:18:51 +0600

    Adds trait for manipulating response

---
+++ app/Traits/NeedsCustomResponse.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\Traits;
+
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+trait NeedsCustomResponse
+{
+    protected function failedValidation(Validator $validator): void
+    {
+        throw new HttpResponseException($this->customJsonResponse(
+            statusCode: JsonResponse::HTTP_UNPROCESSABLE_ENTITY,
+            errors: $validator->errors()
+        ));
+    }
+
+    protected function throwHttpResponseException(
+        int $statusCode = JsonResponse::HTTP_UNPROCESSABLE_ENTITY,
+        mixed $data = [],
+        string $error = null,
+        mixed $errors = []
+    ): JsonResponse {
+        throw new HttpResponseException($this->customJsonResponse(
+            $statusCode,
+            $data,
+            $error,
+            $errors,
+        ));
+    }
+
+    protected function customJsonResponse(
+        int $statusCode = Response::HTTP_OK,
+        mixed $data = [],
+        string $error = null,
+        mixed $errors = []
+    ): JsonResponse {
+        return response()->json([
+            'success' => $statusCode >= 200 && $statusCode <= 299 ? 1 : 0,
+            'data' => $data,
+            'error' => $error,
+            'errors' => $errors,
+        ], $statusCode);
+    }
+}
